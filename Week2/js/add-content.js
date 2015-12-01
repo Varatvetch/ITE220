24045f553945f86b936945340c758f9b82df6415
varatvetch <ritcherit@hotmail.com>
2015-12-01 18:18:34 +0700

    1 12

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,11 +1,31 @@
-var price;
-var quantity;
-var total;
-
-price = 5000;
-quantity = 14;
-total =price * quantity;
+//var price;
+//var quantity;
+//var total;
+				//new Date().getHours()
+//price = 5000;
+//quantity = 14;
+//total =price * quantity;
 
 //document.write("Total is"+total); 
+//var totalPriceEle = document.getElementById("totalPrice");
+//totalPriceEle.textContent = "$" + total;
+
+var products =["T-Shirt", "Notebook", "Wristband"];
+var price = [10, 5, 1];
+var total = 0;
+var total2 = 0;
+
+var productList = document.getElementById("productList");//ชื่อตัวเชื่อม
+productList.innerHTML += "<li>" +products[0] + ": $" + price[0]+"</li>";
+
+var productList = document.getElementById("productList");
+productList.innerHTML += "<li>" +products[1] + ": $" + price[1]+"</li>";
+
+var productList = document.getElementById("productList");
+productList.innerHTML += "<li>" +products[2] + ": $" + price[2]+"</li>";
+
+total = (price[0]+price[1]+price[2])*(80/100);
+
 var totalPriceEle = document.getElementById("totalPrice");
 totalPriceEle.textContent = "$" + total;
+
