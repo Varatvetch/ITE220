54ccc09731abda085bd01e05fa52a032fe30d963
varatvetch <ritcherit@hotmail.com>
2015-12-02 22:37:18 +0700

    New hw calendar

---
+++ Week2-2/js/calendar.js
@@ -0,0 +1,6 @@
+// display the calendar here
+var hotelTable = document.getElementById("hotelCalendarTable");
+var defaultData = hotelTable.innerHTML;
+var stringHTML = "";
+stringHTML = "";
+hotelTable.innerHTML += stringHTML;
