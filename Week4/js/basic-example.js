47a5ba720c19843710b30d4b8dfaedf98c2b9ac9
varatvetch <ritcherit@hotmail.com>
2015-12-16 21:46:29 +0700

    no message

--- Week4/js/basic-example.js
+++ Week4/js/basic-example.js
@@ -1,1 +1,1 @@
-$(':header').addClass('headline');$('li:lt(3)').hide();$('li').on('click', function() {  $(this).remove(); }); $('h1').on('click', function() {  $('li').show(300);});
+$(':header').addClass('headline');$('li:lt(3)').hide();$('li').on('click', function() {  $(this).remove(); }); $('h1').on('click', function() {  $('li').fadeIn(300);});
